140de0a44d6c4012ca12666718c6f5f05a9810f8
Navid Kalaei <navidkalaei@gmail.com>
2016-12-31 18:56:05 +0330

    add simple turn LED on program

---
+++ set_LED_on__ino_/set_LED_on__ino_.ino
@@ -0,0 +1,19 @@
+/**
+ * Set LED on
+ * use this program to reset board
+ *
+ * @author: Navid Kalaei <navidkalaei@gmail.com>
+ */
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+}
