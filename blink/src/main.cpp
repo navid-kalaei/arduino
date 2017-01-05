62b358c7cd38005fd01455d9520a7ed29ba53c4e
Navid Kalaei <navidkalaei@gmail.com>
2017-01-05 22:00:02 +0330

    decorate comments blink/main.cpp

--- blink/src/main.cpp
+++ blink/src/main.cpp
@@ -16,18 +16,36 @@
 
 void setup()
 {
-  // initialize LED digital pin as an output.
+  //////////////////////////////////////////////
+  // initialize LED digital pin as an output. //
+  //////////////////////////////////////////////
+
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
+  /////////////////////////////////////////////////
+  // turn the LED on (HIGH is the voltage level) //
+  /////////////////////////////////////////////////
+
   digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a second
+
+  ///////////////////////
+  // wait for a second //
+  ///////////////////////
+
   delay(1000);
-  // turn the LED off by making the voltage LOW
+
+  ////////////////////////////////////////////////
+  // turn the LED off by making the voltage LOW //
+  ////////////////////////////////////////////////
+
   digitalWrite(LED_BUILTIN, LOW);
-  // wait for a second
+
+  ///////////////////////
+  // wait for a second //
+  ///////////////////////
+
   delay(1000);
 }
