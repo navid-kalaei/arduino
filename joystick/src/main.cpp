c798430ff510d16c06ef0c38e4deeb7bd7a6d6c7
Navid Kalaei <navidkalaei@gmail.com>
2017-01-05 20:01:09 +0330

    get joystick/main to use custom_joystick lib

--- joystick/src/main.cpp
+++ joystick/src/main.cpp
@@ -17,60 +17,6 @@
 #define JOY_PIN_X 0
 #define JOY_PIN_Y 1
 
-// boundries of analogRead()
-#define ANALOG_READ_LOW 0
-#define ANALOG_READ_HIGH 1023
-
-// boundries for normalizing analogRead() from -90 to +90
-#define NORMALIZE_BOUND_LOW -90
-#define NORMALIZE_BOUND_HIGH 90
-// use this number to shift normalize value be between 0 to 180
-#define NORMALIZE_ORIGIN 90
-
-// the value that joystick has at first
-// they're usefull in normalizing
-short int joyXOrigin = 0;
-short int joyYOrigin = 0;
-
-short int joyXOriginNormalized = 0;
-short int joyYOriginNormalized = 0;
-
-short int joyValueX = 0;
-short int joyValueY = 0;
-
-short int joyValueXNormalized = 0;
-short int joyValueYNormalized = 0;
-
-short int normalize(short int value){
-  /*
-   * a wrapper for map built-in function
-   *
-   * @param value: is within analogRead boundries
-   * @return: normalize value within normalize boundries
-   */
-
-   // https://www.arduino.cc/en/Reference/Map
-   // map(value, fromLow, fromHigh, toLow, toHigh)
-   return map(value,
-    ANALOG_READ_LOW, ANALOG_READ_HIGH,
-    NORMALIZE_BOUND_LOW, NORMALIZE_BOUND_HIGH);
-}
-
-void checkBoundries(short int &value){
-  /*
-   * check if value is between normalized boudries or reset it to a boundry
-   *
-   * @param value: to check
-   * @return: void
-   */
-
-   if(value > NORMALIZE_BOUND_HIGH){
-     value = NORMALIZE_BOUND_HIGH;
-   }
-   else if(value < NORMALIZE_BOUND_LOW){
-     value = NORMALIZE_BOUND_LOW;
-   }
-}
 
 void setup(){
   // initialize joystick pins
