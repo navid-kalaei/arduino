68f6dee04d13eb9c2672ae1b766c47772648b152
Navid Kalaei <navidkalaei@gmail.com>
2017-01-06 01:29:03 +0330

    port servo to user custom_joystick

--- servo/src/main.cpp
+++ servo/src/main.cpp
@@ -11,6 +11,7 @@
 
 #include "Arduino.h"
 #include "Servo.h"
+#include "custom_joystick.h"
 
 // time to wait in millis
 #define DELAY_TIME 200
@@ -21,84 +22,31 @@
 
 #define SERVO_PIN 9
 
-// boundries of analogRead()
-#define ANALOG_READ_LOW 0
-#define ANALOG_READ_HIGH 1023
+// servo can turn from 0 to 180 so it's origin is 90
+#define SERVO_ORIGIN 90
 
-// boundries for normalizing analogRead() from -90 to +90
-#define NORMALIZE_BOUND_LOW -90
-#define NORMALIZE_BOUND_HIGH 90
-// use this number to shift normalize value be between 0 to 180
-#define NORMALIZE_ORIGIN 90
-
-// the value that joystick has at first
-// they're usefull in normalizing
-short int joyXOrigin = 0;
-
-short int joyXOriginNormalized = 0;
-
-short int joyValueX = 0;
-
-short int joyValueXNormalized = 0;
+custom_joystick joy;
 
 Servo myServo;
 
 int myServoPos = 0;
 
-short int normalize(short int value){
-  /*
-   * a wrapper for map built-in function
-   *
-   * @param value: is within analogRead boundries
-   * @return: normalize value within normalize boundries
-   */
-
-   // https://www.arduino.cc/en/Reference/Map
-   // map(value, fromLow, fromHigh, toLow, toHigh)
-   return map(value,
-    ANALOG_READ_LOW, ANALOG_READ_HIGH,
-    NORMALIZE_BOUND_LOW, NORMALIZE_BOUND_HIGH);
-}
-
-void checkBoundries(short int &value){
-  /*
-   * check if value is between normalized boudries or reset it to a boundry
-   *
-   * @param value: to check
-   * @return: void
-   */
-
-   if(value > NORMALIZE_BOUND_HIGH){
-     value = NORMALIZE_BOUND_HIGH;
-   }
-   else if(value < NORMALIZE_BOUND_LOW){
-     value = NORMALIZE_BOUND_LOW;
-   }
-}
-
 void setup(){
+  joy.attach_pin_x(JOY_PIN_X);
+  joy.attach_pin_y(JOY_PIN_Y);
+  joy.calibrate();
+
   myServo.attach(SERVO_PIN);
 
-  // initialize joystick pins
-  pinMode(JOY_PIN_X, INPUT);
-
-  joyXOrigin = analogRead(JOY_PIN_X);
-
-  joyXOriginNormalized = normalize(joyXOrigin);
-
   // wait until Serail is not available
-  while(!Serial);
-  Serial.begin(SERIAL_RATE);
+  // while(!Serial);
+  // Serial.begin(SERIAL_RATE);
 }
 
 void loop(){
-  joyValueX = analogRead(JOY_PIN_X);
-  joyValueXNormalized = normalize(joyValueX) - joyXOriginNormalized;
-  checkBoundries(joyValueXNormalized);
-
-  myServoPos = joyValueXNormalized + NORMALIZE_ORIGIN;
+  myServoPos = joy.get_calibrated_x() + SERVO_ORIGIN;
   myServo.write(myServoPos);
-  //Serial.println(myServoPos);
 
-  // delay(DELAY_TIME);
+  //Serial.println(myServoPos);
+  //delay(DELAY_TIME);
 }
