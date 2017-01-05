2eaef541ab365f8b9d145a9ab5d15499e97cb67b
Navid Kalaei <navidkalaei@gmail.com>
2017-01-05 15:31:55 +0330

    init custom_joystick class scheme

--- joystick/lib/custom_joystick.h
+++ joystick/lib/custom_joystick.h
@@ -1,7 +1,51 @@
 class custom_jostick {
 private:
+  short int pinX;
+  short int pinY;
+
+  // joy pin values in its origin
+  int originX;
+  int originY;
+
+  // boundries for normalization
+  int lowBoundry;
+  int highBoundry;
+
+  int normalizedOriginX;
+  int normalizedOriginY;
+
+  int normalizedX;
+  int normalizedY;
+
+  // a wrapper for map built-in function.
+  // get value and map it from analog boundries(0-1023) to joystick boundries
+  int normalize(int value);
 
 public:
   custom_jostick ();
   virtual ~custom_jostick ();
+
+  void attach_pin_x(short int pin);
+  void attach_pin_y(short int pin);
+
+  void set_lower_bound(int value);
+  void set_higher_bound(int value);
+  // init normalized origins.
+  // don't forget to call whenever change eighter low or high boundries.
+  void colibrate();
+  // return raw values of origins
+  short int get_origin_x();
+  short int get_origin_y();
+  // return normalized values of origins
+  int get_normalized_origin_x();
+  int get_normalized_origin_y();
+  // return normalized values read from analog pins
+  int get_normalized_x();
+  int get_normalized_y();
+  // return raw values read from analog pins
+  short int get_value_x();
+  short int get_value_y();
+
+  short int get_direction_x();
+  short int get_direction_y();
 };
