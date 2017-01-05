28bae8ccd8fb3c731cea4e5f30d8569de47474b8
Navid Kalaei <navidkalaei@gmail.com>
2017-01-05 17:16:49 +0330

    update docsting

--- joystick/lib/custom_joystick.cpp
+++ joystick/lib/custom_joystick.cpp
@@ -2,7 +2,7 @@
  * Custom joystick class for to work with joystick                 *
  *                                                                 *
  * Note: YOU MUST ALWAYS COLIBRATE BEFORE FIRST USAGE              *
- *                                                                 *
+ *            AND THRESHOLD IS NORMALIZED                                            *
  * @author: Navid Kalaei <navidkalaei@gmail.com>                   *
  * @license: MIT                                                   *
 *******************************************************************/
